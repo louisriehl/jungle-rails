33908f2046af8ad8f8a6d4643baa1da81d654d2b
Louis Riehl <louisriehl@gmail.com>
2018-11-08 15:31:13 +0000

    added routes for login/out

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,12 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
-  get '/signup' => 'users#new'
-  get '/login'  => 'users#create'
+  get '/login'   => 'sessions#new'
+  post '/login'  => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/signup'  => 'users#new'
+  get '/login'   => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

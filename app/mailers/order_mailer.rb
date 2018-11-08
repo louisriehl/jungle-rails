0d5ab1649bf352f439e3024c62ea9aa15c4f1adf
Louis Riehl <louisriehl@gmail.com>
2018-11-08 18:11:05 +0000

    added default from

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,7 @@
 class OrderMailer < ApplicationMailer
 
+  default from: "no-reply@jungle.com"
+
   def order_receipt
     @user = current_user
     mail(to: @user.email, subject:'This is a test email...')

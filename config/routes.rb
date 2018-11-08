ced8c22ba1eb89d6145f62f3ed8582551962e01c
Louis Riehl <louisriehl@gmail.com>
2018-11-08 16:02:28 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   post '/logout' => 'sessions#destroy'
 
   get '/signup'  => 'users#new'
-  get '/login'   => 'users#create'
+  post '/users'   => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

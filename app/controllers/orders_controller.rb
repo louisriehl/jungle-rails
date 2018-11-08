c600c65714e1abb70be8f59a19cbc724b1b375d5
Louis Riehl <louisriehl@gmail.com>
2018-11-08 19:39:45 +0000

    cart now empties

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
-      # empty_cart!
+      empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
       OrderMailer.order_receipt(order).deliver_now
     else

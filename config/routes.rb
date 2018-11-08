32138540c22ca0604bd4b2370d27c1be7f9df3b9
Louis Riehl <louisriehl@gmail.com>
2018-11-08 14:51:16 +0000

    added routes for signup/login

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  get '/signup' => 'users#new'
+  get '/login'  => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

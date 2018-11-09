06f83ccd6c5dfd51eaf1550b09550b6f61f4a749
Louis Riehl <louisriehl@gmail.com>
2018-11-09 15:27:37 +0000

    added seed reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,36 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+puts "Re-creating Reviews..."
+
+Review.destroy_all
+
+review.create!({
+  user_id: 1,
+  product_id: 1,
+  description: Faker::Hipster.paragraph(2),
+  rating: 3
+})
+
+review.create!({
+  user_id: 1,
+  product_id: 2,
+  description: Faker::Hipster.paragraph(2),
+  rating: 2
+})
+
+review.create!({
+  user_id: 1,
+  product_id: 3,
+  description: Faker::Hipster.paragraph(2),
+  rating: 4
+})
+
+review.create!({
+  user_id: 1,
+  product_id: 4,
+  description: Faker::Hipster.paragraph(2),
+  rating: 5
+})
 
 puts "DONE!"

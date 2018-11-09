3762b4178edef271d2d8401a7adb7005be6945ac
Louis Riehl <louisriehl@gmail.com>
2018-11-09 15:08:36 +0000

    validating reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product , presence: true
+  validates :user , presence: true
+  validates :description , presence: true,
+                           length: { minimum: 10,
+                                     maximum: 250 }
+  validates :rating , presence: true,
+                      numericality: { greater_than_or_equal_to: 0,
+                                      less_than_or_equal_to: 5 }
+
 end

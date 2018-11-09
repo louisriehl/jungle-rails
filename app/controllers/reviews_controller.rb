0f27a67d6491905244944d0e9cbad997185ed2b2
Louis Riehl <louisriehl@gmail.com>
2018-11-09 17:19:28 +0000

    can now add reviews to correct product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,24 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise 'Got post request, but there\'s nothing here!'
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @product = Product.find_by(id: params[:product_id])
+    @review.product = @product
+
+    if @review.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render "/products/#{@product.id}"
+    end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(
+        :rating,
+        :description,
+        :product_id
+      )
+    end
 end

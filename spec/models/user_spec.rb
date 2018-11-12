67dcf4d2bcf12de15833a3488466a5a6e63b48be
Louis Riehl <louisriehl@gmail.com>
2018-11-12 15:56:34 +0000

    added password testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,38 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
 
+    context 'Passwords' do
+
+      it 'should be valid when password and password_confirmation match' do
+        subject.password = '12345'
+        subject.password_confirmation = '12345'
+        expect(subject).to be_valid
+      end
+
+      it 'should be invalid when there is no password' do
+        subject.password = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'should be invalid when there is no password_confirmation' do
+        subject.password_confirmation = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'should be invalid when password and password_confirmation do not match' do
+        subject.password = '12345'
+        subject.password_confirmation = '56789'
+        expect(subject).to_not be_valid
+      end
+
+    end
+
+    context 'Email' do
+
+      it 'should be invalid when there is no email'
+
+    end
+
   end
 
 end

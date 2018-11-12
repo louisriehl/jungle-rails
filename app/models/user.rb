219e21cdfab34c12bf773e470b50bef8c88729bd
Louis Riehl <louisriehl@gmail.com>
2018-11-12 14:55:28 +0000

    added validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :name, presence: true
+
 end

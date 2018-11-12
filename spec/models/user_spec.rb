5aa0b5062a3489c02e874c407ea6ca9735abee82
Louis Riehl <louisriehl@gmail.com>
2018-11-12 14:59:51 +0000

    added validations for valid and no name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { described_class.new(
+    name: 'Billy',
+    password: 'password',
+    password_confirmation: 'password',
+    email: 'myemail@email.com'
+    )}
+
+  context 'Validations' do
+
+    it 'should be valid when fields are valid' do
+      expect(subject).to be_valid
+    end
+
+    it 'should be invalid when name is blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+
 end

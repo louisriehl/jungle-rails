49bbceb96203afec21117445c70780030388a912
Louis Riehl <louisriehl@gmail.com>
2018-11-12 14:57:05 +0000

    added confirmation tag for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :name, presence: true

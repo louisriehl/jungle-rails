9c8529e9549fb901239baf1faaae23551aaf8ae1
Louis Riehl <louisriehl@gmail.com>
2018-11-13 15:50:13 +0000

    load images into spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -13,6 +13,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
           name: Faker::Hipster.sentence(3),
           description: Faker::Hipster.paragraph(4),
           quantity: 10,
+          image: open_asset('apparel1.jpg'),
           price: 64.99
         )
     end

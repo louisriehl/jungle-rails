ad2c375c8037ec7d98334a726a535fde04aa6197
Louis Riehl <louisriehl@gmail.com>
2018-11-13 19:29:15 +0000

    navigate to login and create user

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.feature "User can login from login page", type: :feature, js: true do
 
+  # SETUP
+
+  before :each do
+
+    @user = User.create!(name: 'Test',
+                         password: 'password',
+                         password_confirmation: 'password',
+                         email: 'test@email.com')
+
+  end
+
 
   scenario "They access the login page" do
 
@@ -9,7 +20,7 @@ RSpec.feature "User can login from login page", type: :feature, js: true do
     visit "/login"
 
     # DEBUG
-    screen_shot
+    save_screenshot
 
   end
 
